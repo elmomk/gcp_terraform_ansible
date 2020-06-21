d7d3ef5e5eda3580b9d22ff61f7f4b75f7edef6f
Salomo Bartholomees <be08511@oc6387211014.ibm.com>
2020-06-21 23:22:14 +0200

    tf done

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ resource "google_compute_instance" "default" {
  zone         = "us-west1-a"
  provisioner "local-exec" {
  command = "sleep 15; ansible-playbook -i'${google_compute_instance.default.network_interface.0.access_config.0.nat_ip},' main.yml"
-}  
+                }  
+ tags = ["http-server"]
  metadata = {
    ssh-keys = "centos:${file("~/.ssh/google_compute_engine.pub")}"
  }
@@ -32,11 +33,8 @@ resource "google_compute_instance" "default" {
 boot_disk {
 initialize_params {
      image = "centos-cloud/centos-7"
-   
-}
- 
-}
-
+        }
+    }
 //  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
 
 // metadata_startup_script = "sudo yum -y update; sudo yum install -y "
@@ -49,7 +47,24 @@ access_config {
      // Include this section to give the VM an external ip address
    
 }
- 
+}
 }
 
-}
+// resource "google_compute_firewall" "default" {
+//   name    = "vm-firewall"
+//   network = google_compute_network.default.name
+// 
+// allow {
+//     protocol = "tcp"
+//     ports    = ["80"]
+//     //ports    = ["80", "8080", "1000-2000"]
+//   
+// }
+// 
+//   source_tags = ["web"]
+// 
+// }
+// resource "google_compute_network" "default" {
+//   name = "vm-network"
+// 
+// }
